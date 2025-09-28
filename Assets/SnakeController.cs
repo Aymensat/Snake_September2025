cd7e87d49e6a1cd8aa93cf419a13105b5ea611fc
Aymen Satouri <satouriaymen@gmail.com>
2025-09-28 15:01:07 +0100

    crap aside ,  at least this is bare miminum functional snake

--- Assets/SnakeController.cs
+++ Assets/SnakeController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
@@ -10,8 +11,7 @@ public class SnakeController : MonoBehaviour
     [Header("Prefab objects")]
     [SerializeField]
     private GameObject snakeHeadPrefab;
-    [SerializeField]
-    private GameObject snakeBodyPrefab;
+
     [SerializeField]
     private GameObject snakeFoodPrefab;
 
@@ -25,9 +25,9 @@ public class SnakeController : MonoBehaviour
     [SerializeField]
     private int gridHeight = 30; //30
     [SerializeField]
-    private int initialSpawnX = 10; // within the gridf   
+    private int initialSpawnX = 10; // no used , delibertly starting at 0   
     [SerializeField]
-    private int initialSpawnY = 15;   //within the grid
+    private int initialSpawnY = 15;   //  no used , delibertly starting at 0   
 
 
     //input 
@@ -42,7 +42,7 @@ public class SnakeController : MonoBehaviour
 
 
     //grid related  ;
-    int[,] grid ;  //    5 head    3 body   10 food  15 special fodd  0 empty    -1 barrier
+    int[,] grid ;  //    1 snake  5 food  15 special fodd  0 empty    -1 barrier
     private float cellSizeX; //calcultated not given 
     private float cellSizeY;
 
@@ -54,9 +54,12 @@ public class SnakeController : MonoBehaviour
     float timer = 0;
     int timeUntilMove = 100;
     List<(int x , int y)> snakeArray = new List<(int x, int y)>();
+    List <GameObject> renderedSnakeArray = new List<GameObject> ();
 
 
     GameObject SnakeHead;
+    private GameObject currentFood;
+
     private void Awake()
     {
         worldHeight= Camera.main.orthographicSize * 2f;
@@ -65,7 +68,10 @@ public class SnakeController : MonoBehaviour
         cellSizeX = worldWidth / gridWidth; 
         cellSizeY = worldHeight/ gridHeight;
 
-        grid = new int[gridHeight , gridWidth]; 
+        grid = new int[gridWidth, gridHeight];  // not sure about this , shoudld i rather do grid = new int[gridWidth , gridHeight]; bcz i will like to accss grid[x,y ] ,
+                                                 // bcz usually the standard , like unity inspector starts with x then y , but then again usally in arrays and
+                                                 // matrix we start with lines then columsn , this is confusing 
+                                                 
 
     }
 
@@ -75,7 +81,10 @@ public class SnakeController : MonoBehaviour
         SnakeHead = Instantiate(snakeHeadPrefab , CellToWorld(0 , 0) , Quaternion.identity );
         grid[0, 0] = 1;
         snakeArray.Add((0 , 0)); 
+        renderedSnakeArray.Add( SnakeHead );
         SnakeHead.transform.localScale = new Vector3(cellSizeX , cellSizeY, 0);
+        GenerateFood();
+
 
     }
 
@@ -86,14 +95,7 @@ public class SnakeController : MonoBehaviour
 
         if (direction == MyDirection.right || direction == MyDirection.left)
         {
-            if (vertical == 1) 
-            { 
-                
-                direction = MyDirection.up;
-                GenerateFood();
-            
-            }
-
+            if (vertical == 1)  direction = MyDirection.up;
 
             if (vertical == -1) direction = MyDirection.down;
         }
@@ -108,7 +110,8 @@ public class SnakeController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        MoveSnake(SnakeHead); 
+        MoveSnake(); 
+
     }
 
     private Vector3 CellToWorld( int x , int y)
@@ -121,9 +124,9 @@ public class SnakeController : MonoBehaviour
         return vec3 ;
     }
 
-    private void MoveSnake(GameObject obj)
+    private void MoveSnake()
     {
-
+        //reletex to fix updated which is standard to 0.02 s across all  hardwards 
         if (timer * speed < timeUntilMove)
         {
             timer++;
@@ -131,22 +134,66 @@ public class SnakeController : MonoBehaviour
         else
         {
             timer = 0;
-            if (direction == MyDirection.right) obj.transform.Translate(cellSizeX, 0, 0);
-            else if (direction == MyDirection.left) obj.transform.Translate(-cellSizeX, 0, 0);
-            else if (direction == MyDirection.up) obj.transform.Translate(0, cellSizeY, 0);
-            else if (direction == MyDirection.down) obj.transform.Translate(0, -cellSizeY, 0);
-            
+
+            if (direction == MyDirection.right)
+            {
+
+
+                if (grid[snakeArray[^1].x + 1, snakeArray[^1].y] == 0) ResolveEmptyAhead(snakeArray[^1].x + 1 , snakeArray[^1].y);      // the head always last 
+
+                if (grid[snakeArray[^1].x + 1, snakeArray[^1].y] == 1) ResolveSnakeAhead(snakeArray[^1].x + 1, snakeArray[^1].y);
+
+                if (grid[snakeArray[^1].x + 1, snakeArray[^1].y] == 5) ResolveFoodAhead(snakeArray[^1].x + 1, snakeArray[^1].y);
+
+
+            }
+
+
+            else if (direction == MyDirection.left) 
+            {
+
+                if (grid[snakeArray[^1].x - 1, snakeArray[^1].y] == 0) ResolveEmptyAhead(snakeArray[^1].x - 1, snakeArray[^1].y);      // the head always last 
+
+                if (grid[snakeArray[^1].x - 1, snakeArray[^1].y] == 1) ResolveSnakeAhead(snakeArray[^1].x - 1, snakeArray[^1].y);
+
+                if (grid[snakeArray[^1].x - 1, snakeArray[^1].y] == 5) ResolveFoodAhead(snakeArray[^1].x - 1, snakeArray[^1].y);
+
+            }
+
+            else if (direction == MyDirection.up) 
+            {
+
+                if (grid[snakeArray[^1].x , snakeArray[^1].y + 1] == 0) ResolveEmptyAhead(snakeArray[^1].x , snakeArray[^1].y +1);      // the head always last 
+
+                if (grid[snakeArray[^1].x , snakeArray[^1].y+1] == 1) ResolveSnakeAhead(snakeArray[^1].x , snakeArray[^1].y+1);
+
+                if (grid[snakeArray[^1].x, snakeArray[^1].y+1] == 5) ResolveFoodAhead(snakeArray[^1].x , snakeArray[^1].y+1);
+            }
+
+            else if (direction == MyDirection.down) 
+            {
+                if (grid[snakeArray[^1].x, snakeArray[^1].y - 1] == 0) ResolveEmptyAhead(snakeArray[^1].x, snakeArray[^1].y - 1);      // the head always last 
+
+                if (grid[snakeArray[^1].x, snakeArray[^1].y - 1] == 1) ResolveSnakeAhead(snakeArray[^1].x, snakeArray[^1].y - 1);
+
+                if (grid[snakeArray[^1].x, snakeArray[^1].y - 1] == 5) ResolveFoodAhead(snakeArray[^1].x, snakeArray[^1].y - 1);
+            }
+
+
         }
     }
 
+
+
     private void GenerateFood()
     {
+        Debug.Log("food getting called "); 
         List< ( int x , int y )>  zeroList = new List< ( int x , int y )>();
 
-        for ( int i = 0; i < gridHeight; i++)
+        for ( int i = 0; i < gridWidth; i++)
+        
         {
-        {
-            for ( int j = 0; j < gridWidth; j++)
+            for ( int j = 0; j < gridHeight; j++)
             {
                 if(grid[i, j] == 0) zeroList.Add( ( i , j ) );
             }
@@ -156,16 +203,97 @@ public class SnakeController : MonoBehaviour
 
         var ( x , y) = zeroList[randomIndex];
 
-        grid[x, y] = 2;
+        grid[x, y] = 5; // 5 for food
 
             GameObject food = Instantiate(snakeFoodPrefab, CellToWorld(x, y), Quaternion.identity);
 
             food.transform.localScale = new Vector3(cellSizeX, cellSizeY, 0);
 
+            currentFood = food;
+    }
+
+
+
+    private void ResolveEmptyAhead(int x, int y)
+    {
+        //grid update
+
+        grid[x, y] = 1;
+        grid[snakeArray[0].x , snakeArray[0].y] = 0;
+
+
+
+        //case of just head moving 
+        if (snakeArray.Count == 1)
+        {
+
+            snakeArray[0] = (x, y);
+
+            renderedSnakeArray[0].transform.position =  CellToWorld(x, y) ;
+
+            return; //how to test ResolveEmptyAhead before implementing Resolve food ?
+
         }
 
 
+        // logic array management
+
+        snakeArray.Add((x,y));
+        snakeArray.RemoveAt(0);
+
+
+        // GO manamgemnt
+
+
+        GameObject tail = renderedSnakeArray[0];   //getting the  tail to reuse it 
+        renderedSnakeArray.RemoveAt(0);    
+
+        tail.transform.position = CellToWorld(x, y);
+        tail.GetComponent<SpriteRenderer>().color = Color.red;
+        renderedSnakeArray.Add(tail);
+
+        renderedSnakeArray[^2].GetComponent<SpriteRenderer>().color = Color.blue; // rechagint the old head into normal body
+
+
+
+        
+
+
+
     }
+    private void ResolveFoodAhead(int x, int y)
+    {
+        //grid management
+        grid[x,y] = 1;
+
+        //array management
+
+        snakeArray.Add((x,y));
+
+        //GO management
+
+        GameObject newHead = Instantiate(SnakeHead, CellToWorld(x, y), Quaternion.identity);
+        renderedSnakeArray[^1].GetComponent<SpriteRenderer>().color = Color.blue;
+        renderedSnakeArray.Add(newHead);
+
+        //generate food 
+
+        Destroy(currentFood);
+        GenerateFood(); 
+    }
+
+    private void ResolveBarrierAhead(int x, int y)
+    {
+        // TO DO
+    }
+
+    private void ResolveSnakeAhead(int x, int y)
+    {
+        // TO DO
+    }
+
+    //TO DO warping/OOB manamagent
+
 
 } 
 
